111b24c827ebd65ec7997057696ad95705a94d6a
jeffyanta <jeff@flipcash.com>
2026-01-28 11:40:52 -0500

    Add custom push payload

--- generated/protobuf-es/index.ts
+++ generated/protobuf-es/index.ts
@@ -1,6 +1,7 @@
 export * as Activity from './activity/v1';
 export * as Activity from './activity/v1';
 export * as Push from './push/v1';
+export * as Push from './push/v1';
 export * as Iap from './iap/v1';
 export * as Common from './common/v1';
 export * as Profile from './profile/v1';
--- generated/protobuf-es/push/v1/index.ts
+++ generated/protobuf-es/push/v1/index.ts
@@ -1,2 +1,3 @@
 export * from './push_service_connect';
 export * from './push_service_pb';
+export * from './model_pb';
