79a6ecdf0aede5ce8bda44fdb61beaa909439172
jeffyanta <jeff@flipcash.com>
2026-06-09 12:37:05 -0400

    Add AppMetadata for intents

--- generated/protobuf-es/index.ts
+++ generated/protobuf-es/index.ts
@@ -9,6 +9,7 @@ export * as Push from './push/v1';
 export * as Push from './push/v1';
 export * as Iap from './iap/v1';
 export * as Common from './common/v1';
+export * as Intent from './intent/v1';
 export * as Profile from './profile/v1';
 export * as Profile from './profile/v1';
 export * as Phone from './phone/v1';
