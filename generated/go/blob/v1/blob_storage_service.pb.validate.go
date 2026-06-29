f1014792ec00e1f15f83c2cfaae42e55b32f7358
jeffyanta <jeff@flipcash.com>
2026-06-29 16:30:49 -0400

    Add RejectionMetadata to CompleteExternalUploadResponse (#59)

--- generated/go/blob/v1/blob_storage_service.pb.validate.go
+++ generated/go/blob/v1/blob_storage_service.pb.validate.go
@@ -876,6 +876,35 @@ func (m *CompleteExternalUploadResponse) validate(all bool) error {
 
 	// no validation rules for Status
 
+	if all {
+		switch v := interface{}(m.GetRejectionMetadata()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, CompleteExternalUploadResponseValidationError{
+					field:  "RejectionMetadata",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, CompleteExternalUploadResponseValidationError{
+					field:  "RejectionMetadata",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetRejectionMetadata()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return CompleteExternalUploadResponseValidationError{
+				field:  "RejectionMetadata",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return CompleteExternalUploadResponseMultiError(errors)
 	}
