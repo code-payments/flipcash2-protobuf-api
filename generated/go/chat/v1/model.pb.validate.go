814070584ec931b5ab50f8cb811ca912bfdfe877
jeffyanta <jeff@flipcash.com>
2026-09-13 23:58:54 -0400

    Fix RosterSummary validation

--- generated/go/chat/v1/model.pb.validate.go
+++ generated/go/chat/v1/model.pb.validate.go
@@ -237,6 +237,35 @@ func (m *Metadata) validate(all bool) error {
 		errors = append(errors, err)
 	}
 
+	if all {
+		switch v := interface{}(m.GetRosterSummary()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, MetadataValidationError{
+					field:  "RosterSummary",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, MetadataValidationError{
+					field:  "RosterSummary",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetRosterSummary()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return MetadataValidationError{
+				field:  "RosterSummary",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return MetadataMultiError(errors)
 	}
